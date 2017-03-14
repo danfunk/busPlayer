8a97327ba3044a31fb03744d30ae6b4726c2071e
Dan Funk <daniel.h.funk@gmail.com>
2017-03-14 17:27:35 -0400

    Using relative paths so we can move this stuff around a bit.

--- scan.sh
+++ scan.sh
@@ -1,5 +1,5 @@
 while :
 do
-	sudo beacon scan -A -b -d 4| /home/pi/Desktop/test3.sh
+	sudo beacon scan -A -b -d 4| ./test3.sh
 	sleep 1
 done
--- test3.sh
+++ test3.sh
@@ -4,16 +4,16 @@
 while read line
 do
 		if [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA5 0 1"* ]]; then
-		  mplayer /home/pi/Desktop/01intro.mp3 else
+		  mplayer ./01intro.mp3 else
 		elif [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6 0 1"* ]]; then
-		  mplayer /home/pi/Desktop/02togravesite.mp3 else
+		  mplayer ./02togravesite.mp3 else
 		elif [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA7 0 1"* ]]; then
-		  mplayer /home/pi/Desktop/03gravesite.mp3 else
+		  mplayer ./03gravesite.mp3 else
 		elif [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA8 0 1"* ]]; then
-		  mplayer /home/pi/Desktop/04visitorcenter.mp3 else
+		  mplayer ./04visitorcenter.mp3 else
 		elif [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA9 0 1"* ]]; then
-		  mplayer /home/pi/Desktop/05conclusion.mp3   
+		  mplayer ./05conclusion.mp3   
 		else
-		sudo beacon scan -A -b -d 4| /home/pi/Desktop/test3.sh
+		sudo beacon scan -A -b -d 4| ./test3.sh
 	fi
 done
