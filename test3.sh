9e083ffbdb7dc7a200e877ce21a98ea6d2491650
Dan Funk <daniel.h.funk@gmail.com>
2017-03-16 10:56:50 -0400

    Causing a delay of one minute between playing the same audio again.

--- test3.sh
+++ test3.sh
@@ -1,18 +1,31 @@
 #!/bin/bash	
 
+# Number of minutes to wait before playing the same recording a second time.
+repeatAfterMinutes=1
+
+play () {
+    if [ "$1" == "$(cat lastPlayed.txt)" ] && [ test `find "lastPlayed.txt" -mmin +$repeatAfterMinutes` ] ;then
+      echo "Skipping $1, not repeating again for $repeatAfterMinutes minutes"
+      sleep 5
+    else
+      mplayer $1
+      echo $1 > lastPlayed.txt
+    fi
+}
+
 
 while read line
 do
 		if [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA5 0 1"* ]]; then
-		  mplayer ./01intro.mp3 else
+		  play ./01intro.mp3 
 		elif [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6 0 1"* ]]; then
-		  mplayer ./02togravesite.mp3 else
+		  play ./02togravesite.mp3 
 		elif [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA7 0 1"* ]]; then
-		  mplayer ./03gravesite.mp3 else
+		  play ./03gravesite.mp3 
 		elif [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA8 0 1"* ]]; then
-		  mplayer ./04visitorcenter.mp3 else
+		  play ./04visitorcenter.mp3 
 		elif [[ $line = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA9 0 1"* ]]; then
-		  mplayer ./05conclusion.mp3   
+		  play ./05conclusion.mp3   
 		else
 		sudo beacon scan -A -b -d 4| ./test3.sh
 	fi
