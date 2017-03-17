5ff402716d046c16be76d8cc42369cccb3761e75
Dan Funk <daniel.h.funk@gmail.com>
2017-03-16 22:53:32 -0400

    Modified the script to execute more cleanly as a single file.  Added a busplayer init.d script that will allow the system to start and manage the service rather than trying to run it from the bash init scripts.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # busPlayer
+
+
+## Install
+* Place the busplayer script into /etc/init.d
+     sudo cp busplayer /etc/init.d/
+* Make it executable 
+     sudo chmod 755 /etc/init.d/busplayer
+* Make sure that scan.sh and playSound.sh are executable
+     chmod 755 scan.sh
+     chmod 755 paySound.sh
+* Instruct the OS to start the busplayer script on startup
+     sudo update-rc.d busplayer defaults
--- scan.sh
+++ scan.sh
@@ -1,5 +1,33 @@
+#!/bin/bash	
+
+# Number of minutes to wait before playing the same recording a second time.
+repeatAfterMinutes=1
+
+play () {
+    if [ "$1" == "$(cat lastPlayed.txt)" ] && [ test `find "lastPlayed.txt" -mmin +$repeatAfterMinutes` ] ;then
+      echo "Skipping $1, not repeating again for $repeatAfterMinutes minutes"
+      sleep 5
+    else
+      mplayer $1
+      echo $1 > lastPlayed.txt
+    fi
+}
+
+
 while :
 do
-	sudo beacon scan -A -b -d 4| ./playSound.sh
+    beacon_id=`sudo beacon scan -A -b -d 4`
+    if [[ $beacon_id = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA5 0 1"* ]]; then
+	play ./01intro.mp3 
+    elif [[ $beacon_id = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6 0 1"* ]]; then
+	play ./02togravesite.mp3 
+    elif [[ $beacon_id = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA7 0 1"* ]]; then
+	play ./03gravesite.mp3 
+    elif [[ $beacon_id = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA8 0 1"* ]]; then
+	play ./04visitorcenter.mp3 
+    elif [[ $beacon_id = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA9 0 1"* ]]; then
+	play ./05conclusion.mp3   
+    else
 	sleep 1
+    fi
 done
